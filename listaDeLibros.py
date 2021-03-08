01b11ed6ca5cf312924cb4ee3a8b8c267ace4f34
DESKTOP-5154QTN\marif <maria.salas.miranda@ucreativa.com>
2021-03-08 16:58:16 -0600

    Imprimir lista de libros

--- listaDeLibros.py
+++ listaDeLibros.py
@@ -1,8 +1,12 @@
-from xlrd import open_workbook
+from cargarDatos import cargar_lista_de_libros, cargar_lista_de_personas
+from utils import imprimir_libro, imprimir_persona
+
+def mostrar_titulo():
+    print("*********************************************")
+    print("**             Lista de libros             **")
+    print("*********************************************")
 
 def imprimir_lista_de_libros():
-    path = "data/libros.xlsx"
-    wb = open_workbook(path)
-    sh1 = wb.sheet_by_index(0)
-    for i in range(sh1.nrows):
-        print(sh1.cell_value(i, 0) + "      " + sh1.cell_value(i, 1))
+    lista_de_libros = cargar_lista_de_libros()
+    mostrar_titulo()
+    print(*map(imprimir_libro, lista_de_libros))
