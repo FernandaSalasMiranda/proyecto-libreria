3b4da3fbcfb3f3c5a253e2f7c44bce62dcdc2ee4
DESKTOP-5154QTN\marif <maria.salas.miranda@ucreativa.com>
2021-03-08 16:57:38 -0600

    Cargar lista de libros

--- cargarDatos.py
+++ cargarDatos.py
@@ -8,3 +8,15 @@ def cargar_lista_de_personas():
     for i in range(sh1.nrows):
         lista_de_personas.append({"id": sh1.cell_value(i, 0), "nombre": sh1.cell_value(i, 1), "correo": sh1.cell_value(i, 2)})
     return lista_de_personas
+
+
+def cargar_lista_de_libros():
+    path = "data/libros.xlsx"
+    wb = open_workbook(path)
+    sh1 = wb.sheet_by_index(0)
+    lista_de_libros = []
+    for i in range(sh1.nrows):
+        lista_de_libros.append(
+            {"id": str(sh1.cell_value(i, 0)), "titulo": str(sh1.cell_value(i, 1)), "genero": sh1.cell_value(i, 2),
+             "autor": sh1.cell_value(i, 3)})
+    return lista_de_libros
