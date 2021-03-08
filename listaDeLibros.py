46b622a62bf4376b52c657f0f169855296f6e5c1
DESKTOP-5154QTN\marif <maria.salas.miranda@ucreativa.com>
2021-03-08 17:26:55 -0600

    Buscar libro

--- listaDeLibros.py
+++ listaDeLibros.py
@@ -10,3 +10,9 @@ def imprimir_lista_de_libros():
     lista_de_libros = cargar_lista_de_libros()
     mostrar_titulo()
     print(*map(imprimir_libro, lista_de_libros))
+
+
+def buscar_libro(val):
+    lista_de_libros = cargar_lista_de_libros()
+    resultado = filter(lambda libro: val in libro['id'].lower() or val in libro["titulo"].lower() or val in libro["genero"].lower() or val in libro["autor"].lower(), lista_de_libros)
+    print(*map(imprimir_libro, resultado))
